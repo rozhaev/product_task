975d7b3960c1e6616c6ab93f883dbc46adacaa3a
Dmitry Rozhaev <dmitry.rozhaev@gmail.com>
2020-09-02 15:14:34 +0300

    created the endpoints for the product controller

--- src/main/java/com/sparkequation/spring/trial/api/controller/ProductController.java
+++ src/main/java/com/sparkequation/spring/trial/api/controller/ProductController.java
@@ -1,12 +1,11 @@
 package com.sparkequation.spring.trial.api.controller;
 
+import com.sparkequation.spring.trial.api.dto.ProductDTO;
 import com.sparkequation.spring.trial.api.model.Product;
 import com.sparkequation.spring.trial.api.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +18,27 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<List<Product>> getProducts() {
+        return ResponseEntity.ok(productService.getProducts());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProducts() {
         return ResponseEntity.ok(productService.GetProducts());
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(ProductDTO productDTO) {
+        return ResponseEntity.ok(productService.createProduct(productDTO));
+    }
+
+    @PutMapping
+    public ResponseEntity<Product> updateProduct(ProductDTO productDTO) {
+        return ResponseEntity.ok(productService.updateProduct(productDTO));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Long> deleteProduct(int id) {
+        boolean isCreated = productService.deleteProduct(id);
+        return isCreated ? ResponseEntity.ok() : ResponseEntity.notFound();
     }
 }
