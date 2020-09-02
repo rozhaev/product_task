41112b0caafa56624ad0f81fdeac09efc927be13
Dmitry Rozhaev <dmitry.rozhaev@gmail.com>
2020-09-02 15:12:47 +0300

    created Product Service interfaces

--- src/main/java/com/sparkequation/spring/trial/api/service/ProductService.java
+++ src/main/java/com/sparkequation/spring/trial/api/service/ProductService.java
@@ -1,9 +1,19 @@
 package com.sparkequation.spring.trial.api.service;
 
+import com.sparkequation.spring.trial.api.dto.ProductDTO;
 import com.sparkequation.spring.trial.api.model.Product;
 
 import java.util.List;
 
 public interface ProductService {
-    List<Product> GetProducts();
+
+    List<Product> getProducts();
+
+    Product getProductById(int id);
+
+    Product createProduct(ProductDTO productDTO);
+
+    Product updateProduct(ProductDTO productDTO);
+
+    boolean deleteProduct(int id);
 }
